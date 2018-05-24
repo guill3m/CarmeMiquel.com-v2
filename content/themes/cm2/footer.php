b37a44e22d0725ca5708400719c53111a6078a09
Guillem Andreu <git@guillemandreu.com>
2018-05-25 00:02:10 +0200

    GDPR: Add link to Privacy Policy page in the footer

--- content/themes/cm2/footer.php
+++ content/themes/cm2/footer.php
@@ -7,7 +7,7 @@
 
 		<footer id="footer" class="footer">
 			<small>
-				<span class="footer-left">© Carme Miquel, <?php echo date('Y'); ?></span>
+				<span class="footer-left">© Carme Miquel, <?php echo date('Y'); ?>. <a href="https://carmemiquel.com/politica-de-privadesa/">Política de privadesa</a>.</span>
 				<span class="footer-right">Disseny Web: <a href="http://guillemandreu.com/">Guillem Andreu</a></span>
 			</small>
 		</footer><!-- END #footer -->
